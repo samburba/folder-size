ffd60dd205eb7e51eed7cd9c385ef34ab50432a7
Sam Burba <sam@burba.email>
2022-01-16 18:22:01 -0800

    formatted

--- cmd/folder-size/main.go
+++ cmd/folder-size/main.go
@@ -5,22 +5,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"strings"
 	"sort"
+	"strings"
 )
 
 func ByteCountSI(b int64) string {
-    const unit = 1000
-    if b < unit {
-        return fmt.Sprintf("%d B", b)
-    }
-    div, exp := int64(unit), 0
-    for n := b / unit; n >= unit; n /= unit {
-        div *= unit
-        exp++
-    }
-    return fmt.Sprintf("%.1f %cB",
-        float64(b)/float64(div), "kMGTPE"[exp])
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
 }
 
 func GetFolderSize(directory string, size int64) int64 {
@@ -31,7 +31,7 @@ func GetFolderSize(directory string, size int64) int64 {
 	for _, f := range files {
 		size = f.Size() + size
 		if f.IsDir() {
-			size = GetFolderSize(directory + "/" + f.Name(), size)
+			size = GetFolderSize(directory+"/"+f.Name(), size)
 		} else {
 			return size
 		}
@@ -48,12 +48,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	folders := make(map[string]int64)
-	
+
 	for _, f := range files {
 		if f.IsDir() {
-			size := GetFolderSize(*directoryPtr + "/" + f.Name(), 0)
+			size := GetFolderSize(*directoryPtr+"/"+f.Name(), 0)
 			folders[f.Name()] = size
 		} else {
 			folders[f.Name()] = f.Size()
@@ -65,7 +65,7 @@ func main() {
 		names = append(names, k)
 	}
 
-	sort.Slice(names, func(i, j int) bool { 
+	sort.Slice(names, func(i, j int) bool {
 		return folders[names[i]] > folders[names[j]]
 	})
 	top := folders[names[0]]
@@ -74,10 +74,10 @@ func main() {
 	if *visualizationPtr {
 		for _, name := range names {
 			if folders[name] == 0 {
-				fmt.Println(strings.Repeat(" ", spacing + 2), name, ByteCountSI(folders[name]))
+				fmt.Println(strings.Repeat(" ", spacing+2), name, ByteCountSI(folders[name]))
 			} else {
-				repeat := (spacing * int(folders[name])/int(top)) + 1
-				fmt.Println(strings.Repeat("#", repeat), strings.Repeat(" ", spacing + 1 - repeat), name, ByteCountSI(folders[name]))
+				repeat := (spacing * int(folders[name]) / int(top)) + 1
+				fmt.Println(strings.Repeat("#", repeat), strings.Repeat(" ", spacing+1-repeat), name, ByteCountSI(folders[name]))
 			}
 		}
 	} else {
